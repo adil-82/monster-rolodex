69eeb6bbcf0deaf06a5bad7be2fdba2d08cbc42a
adil-82 <bouaddiadil82@gmail.com>
2020-06-22 14:04:02 +0100

    Updates

---
+++ precache-manifest.81cc39893244976deeaacda623cd3522.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ac3a50f324951c1f54537d922db85ab",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "353d19098943df3a6623",
+    "url": "/monster-rolodex/static/css/main.f5b86b50.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "353d19098943df3a6623",
+    "url": "/monster-rolodex/static/js/main.ac1ffc49.chunk.js"
+  },
+  {
+    "revision": "212570b3a5f291f8b8f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a41f6c3.js"
+  }
+]);
